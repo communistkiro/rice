2960cd3993bcf546310ef1488d6b7ea1fe3d05cd
communistkiro <vlg@tutamail.com>
2020-04-24 02:55:42 +0300

    fixed 24h time axis

--- .config/zsh/autoloadmedaddy/wdr.m
+++ .config/zsh/autoloadmedaddy/wdr.m
@@ -42,12 +42,9 @@ set (ax, "linewidth", 2);
 set (gca, 'fontsize',14,'fontweight','bold'); 
 
 
-% figure 2;
 subplot (3, 3, 7:9);
-plot (dt48h, clouds48h, "k;Cloud coverage, %;", "linewidth", 2); hold on; [ax, h1, h2] = plotyy (dt48h, humidity48h, dt48h, pressure48h); ylabel (ax(1), "Relative humidity, %"); ylabel (ax(2), "Pressure AGL, mbar"); for a=[1 2]; xlabel (ax(a), 'UTC'); endfor;
-grid minor; axis tight;
-dateaxis ('x', '%d.');
-% title ('48h forecast');
+plot (dt48h, clouds48h, "k;Cloud coverage, %;", "linewidth", 2); dateaxis ('x', '%d.'); hold on; [ax, h1, h2] = plotyy (dt48h, humidity48h, dt48h, pressure48h); ylabel (ax(1), "Relative humidity, %"); ylabel (ax(2), "Pressure AGL, mbar"); for a=[1 2]; xlabel (ax(a), 'UTC'); endfor;
+grid minor; axis tight; dateaxis (ax(1), 'x', '%d.'); dateaxis (ax(2), 'x', '%d.');
 set (ax(:),'fontsize', 14, 'fontweight', 'bold');
 set ([h1 h2], 'linewidth', 2);
 
@@ -61,11 +58,9 @@ set (ax, "linewidth", 2);
 set (gca, 'fontsize',14,'fontweight','bold'); 
 
 
-% figure 4;
 subplot (3, 3, 7:9);
 plot (1:8, clouds7d, 'k;Cloud coverage, %;', "linewidth", 2); hold on; [ax, h1, h2] = plotyy (1:8, humidity7d, 1:8, pressure7d); ylabel (ax(1), 'Relative humidity, %'); ylabel (ax(2), 'Pressure AGL, mbar'); for a=[1 2]; xlabel (ax(a), 'UTC'); endfor
 grid minor; axis tight;
-% title ('7d forecast');
 set (ax(:),'fontsize', 14, 'fontweight', 'bold');
 set ([h1 h2], 'linewidth', 2);
 
