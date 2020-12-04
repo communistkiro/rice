94fb2860d0ea33b12e33f213eb23b8d2d1a026e0
communistkiro <vlg@tutamail.com>
2020-12-04 17:43:08 +0200

    speedreading in sfeed curses working bandcampddisco fix

--- src/slock/config.h
+++ src/slock/config.h
@@ -5,7 +5,7 @@ static const char *group = "nogroup";
 static const char *colorname[NUMCOLS] = {
 	[INIT] =   "black",     /* after initialization */
 	[INPUT] =  "#000000",   /* during input */
-	[FAILED] = "#FFFFFF",   /* wrong password */
+	[FAILED] = "#222222",   /* wrong password */
 };
 
 /* treat a cleared input like a wrong password (color) */
